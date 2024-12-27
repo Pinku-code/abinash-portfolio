2dbf802b343f26bdea21e365b1a175d5e449dbc5
Pinku-code <ns4507262@gmail.com>
2024-12-27 10:28:52 +0530

    Test

--- src/components/molecules/Banner/index.js
+++ src/components/molecules/Banner/index.js
@@ -55,7 +55,7 @@ const Banner = () => {
 
         <div className="flex items-center translate-y-[-60%] sm:translate-y-[-0%]">
           <a
-            href="https://drive.google.com/file/d/19rnbukAhf9oPhadMhsvI3xnWF6FIYeMT/view?usp=share_link"
+            href="https://drive.google.com/file/d/11Xw-bi0p8ffLT1nlKVRBrmcrsn6H1Kut/view?usp=drive_link"
             target="blank"
           >
             <button className="primary-button">
